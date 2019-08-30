9fa85f238eff2202558dc2fa340cc056fd5abcf7
francescoafabozzi <francescoafabozzi@gmail.com>
2019-08-30 14:15:49 -0400

    Adding Tan Graph

--- DaveDebreceni_Assignment1.py
+++ DaveDebreceni_Assignment1.py
@@ -11,9 +11,11 @@ import matplotlib.pyplot as plt
 x = np.arange(0,2*np.pi,0.1)
 sindata = np.sin(x)
 cosdata = np.cos(x)
+tandata=np.tan(x)
 
 plt.plot(x,sindata, label='Sin(x)')
 plt.plot(x,cosdata, label ='Cos(x)')
-plt.title("Cos and Sin Graph for 1 cycle")
+plt.plot(x,tandata,label='Tan(x)')
+plt.title("Cos, Sin, and Tan Graph for 1 cycle")
 plt.legend()
 plt.show()
