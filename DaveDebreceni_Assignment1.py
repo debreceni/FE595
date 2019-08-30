f62ebe9e9fc2338fb63f351bd4d408a3832a28a2
debreceni <dave@debreceni.net>
2019-08-30 12:25:16 -0400

    Add files via upload

---
+++ DaveDebreceni_Assignment1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 12:11:28 2019
+
+@author: Dave
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0,2*np.pi,0.1)
+sindata = np.sin(x)
+cosdata = np.cos(x)
+
+plt.plot(x,sindata, label='Sin(x)')
+plt.plot(x,cosdata, label ='Cos(x)')
+plt.title("Cos and Sin Graph for 1 cycle")
+plt.legend()
+plt.show()
